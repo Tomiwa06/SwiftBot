c75104f1498f6bf39f1073ce4becf7d1582681c3
Tomiwa06 <82250700+Tomiwa06@users.noreply.github.com>
2026-03-28 14:54:17 +0100

    Update constants.ts

--- constants.ts
+++ constants.ts
@@ -1,7 +1,7 @@
 import { ServiceItem, ServiceType } from './types';
 
 // Direct logo URL from SwiftBooks
-export const LOGO_URL = "/https://ibb.co/q363hgwP"; // after putting logo.png in your /public folder
+export const LOGO_URL = "/https://i.ibb.co/4nknCSx5/logo.png"; // after putting logo.png in your /public folder
 
 // Placeholder for user avatar if needed
 export const USER_AVATAR = "https://picsum.photos/seed/user/200";
